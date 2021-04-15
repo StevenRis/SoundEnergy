dc882ea7e7deca6450ef294bb0ae1a768c05f0ee
trader TGN <trader@tcn.local>
2021-04-15 10:13:57 +0200

    15.04.

---
+++ script.js
@@ -0,0 +1,9 @@
+// --------SIDE MENU
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
